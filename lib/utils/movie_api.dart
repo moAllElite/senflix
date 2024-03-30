c3bdaaf5990e83965518452dcf07bf614329c7cf
NIANG MOUHAMED <mouhamedn347@gmail.com>
2024-03-30 00:55:46 +0000

    upcoming-movies

--- lib/utils/movie_api.dart
+++ lib/utils/movie_api.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:senflix/api_key/api_key.dart';
 import 'package:senflix/models/movie_result.dart';
-import 'package:senflix/models/result.model.dart';
 import 'package:senflix/utils/constant.dart';
 
 
@@ -15,18 +14,18 @@ Future<MovieResult?> searchMovieByName() async {
   if (response.statusCode == 200) {
     var jsonResponse =
     convert.jsonDecode(response.body) as Map<String, dynamic>;
-    var itemCount = jsonResponse['results'][0]['original_title'];
-    //print(itemCount);
+     //print(itemCount);
    // print(Results.fromJson(jsonResponse).popularity);
     return MovieResult.fromJson(jsonResponse);
   } else {
     print('Request failed with status: ${response.statusCode}.');
+
+    return null;
   }
 }
 
 Future<MovieResult?> upcommingMovies() async {
   var url = Uri.tryParse(upcoming);
-  List movies = [];
   // Await the http get response, then decode the json-formatted response.
   var response = await http.get(
     url!,
@@ -37,12 +36,13 @@ Future<MovieResult?> upcommingMovies() async {
   if (response.statusCode == 200) {
     var jsonResponse =
     convert.jsonDecode(response.body) as Map<String, dynamic>;
-    var title = jsonResponse['total_results'];
     MovieResult.fromJson(jsonResponse).results.forEach((element) {
       print(element.posterPath);
     });
     return MovieResult.fromJson(jsonResponse);
   } else {
     print('Request failed with status: ${response.statusCode}.');
+    return null;
   }
+
 }
